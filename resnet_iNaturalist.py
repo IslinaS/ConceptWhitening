97919cba6dc72723e951ec6d58663e943c8f462c
alexkato29 <akbd256@gmail.com>
2024-03-06 14:24:32 -0500

    Added ResNet

---
+++ resnet_iNaturalist.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+from resnet import res50
+
+"""
+Backbone weights pretrained on iNaturalist from BBN paper
+"""
+
+# Import the weights
+weights_path = "models/iNaturalist_180epoch_best.pth"
+inat_weights = torch.load(weights_path)
+
+# Open the model
+model = res50(pretrain=False)
+model.load_model(pretrain=weights_path)    
+print(model)    

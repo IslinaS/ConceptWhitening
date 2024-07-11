0208fd7fb5b5b0f144689324e714c69389b4d3f0
viethungle-vt1401 <brandon.viethungRI2001@gmail.com>
2024-07-11 03:49:19 -0400

    Save intermediate X_rot

--- models/IterNorm.py
+++ models/IterNorm.py
@@ -125,6 +125,10 @@ class IterNormRotation(torch.nn.Module):
         self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=3, return_indices=True)
         self.maxunpool = torch.nn.MaxUnpool2d(kernel_size=3, stride=3)
 
+        # intermediate values for hook extraction
+        self.current_batch_X_test = None
+        self.current_batch_X_rot_activated = None
+
         # running mean
         self.register_buffer('running_mean', torch.zeros(1, num_channels, 1))
         # running whiten matrix
@@ -211,6 +215,8 @@ class IterNormRotation(torch.nn.Module):
             # Throughout this code, d = dimensionality of latent space, self.mode = index of current concept
             if self.mode >= 0:
                 X_redacted = redact(X_hat, X_redact_coords, orig_x_dim)
+                X_test = torch.einsum('bchw,dc->bdhw', X_redacted, self.running_rot)
+                self.current_batch_X_test = X_test
 
                 # Applying the concept activation function
                 if self.activation_mode == 'mean':
@@ -219,7 +225,6 @@ class IterNormRotation(torch.nn.Module):
                     X_activated = X_redacted.sum((2, 3)) / (redact_bool.sum((2, 3)) + 0.0001)
 
                 elif self.activation_mode == 'max':
-                    X_test = torch.einsum('bchw,dc->bdhw', X_redacted, self.running_rot)
                     # bdhw
                     max_values = torch.max(torch.max(X_test, 2, keepdim=True)[0], 3, keepdim=True)[0]
                     max_bool = (max_values == X_test).to(X_redacted)
@@ -227,14 +232,12 @@ class IterNormRotation(torch.nn.Module):
                     X_activated = (X_redacted * max_bool).sum((2, 3)) / max_bool.sum((2, 3))
 
                 elif self.activation_mode == 'pos_mean':
-                    X_test = torch.einsum('bchw,dc->bdhw', X_redacted, self.running_rot)
                     # bdhw
                     pos_bool = (X_test > 0).to(X_redacted)
                     # bd
                     X_activated = (X_redacted * pos_bool).sum((2, 3)) / (pos_bool.sum((2, 3)) + 0.0001)
 
                 elif self.activation_mode == 'pool_max':
-                    X_test = torch.einsum('bchw,dc->bdhw', X_redacted, self.running_rot)
                     # bdhw
                     maxpool_value, maxpool_indices = self.maxpool(X_test)
                     # bdhw
